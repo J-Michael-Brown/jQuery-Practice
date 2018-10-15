e4f01810872f1ef82edafabc7cd731b4c56c589f
Michael Brown and Mary McMurray <michael@>
2018-10-15 13:33:43 -0700

    add inital files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+}); // the $ is an abreviation of "jQuery" for ease of use //
